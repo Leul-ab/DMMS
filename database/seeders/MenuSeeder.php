4f5c2e2a08695f01a9179d032c8441ceaee5d158
Leul-ab <leulabera321@gmail.com>
2026-08-14 09:52:13 +0300

    menu seeder fixed

--- database/seeders/MenuSeeder.php
+++ database/seeders/MenuSeeder.php
@@ -6,7 +6,6 @@ use App\Models\Branch;
 use App\Models\MenuCategory;
 use App\Models\MenuItem;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Str;
 
 class MenuSeeder extends Seeder
 {
@@ -26,8 +25,8 @@ class MenuSeeder extends Seeder
 
         foreach ($categories as $data) {
             MenuCategory::firstOrCreate(
-                ['slug' => Str::slug($data['name'])],
-                [...$data, 'branch_id' => $branch?->id]
+                ['name' => $data['name'], 'branch_id' => $branch?->id],
+                $data
             );
         }
 
@@ -71,10 +70,10 @@ class MenuSeeder extends Seeder
         ];
 
         foreach ($items as $data) {
-            $category = MenuCategory::where('slug', Str::slug($data['category']))->first();
+            $category = MenuCategory::where('name', $data['category'])->where('branch_id', $branch?->id)->first();
             if ($category) {
                 MenuItem::firstOrCreate(
-                    ['slug' => Str::slug($data['name'])],
+                    ['name' => $data['name'], 'branch_id' => $branch?->id, 'category_id' => $category->id],
                     [
                         'branch_id' => $branch?->id,
                         'category_id' => $category->id,
